9433ca171d24f343d77774a971c79f27b087d284
Paul Hoffman <nkuitse@nkuitse.com>
2019-05-18 05:30:46 -0400

    add sample plugin

---
+++ plugins/hello.pm
@@ -0,0 +1,25 @@
+package Ei::Plugin::hello;
+
+use strict;
+use warnings;
+
+sub new {
+    my $cls = shift;
+    bless { @_ }, $cls;
+}
+
+sub commands {
+    return (
+        'hello' => \&hello,
+    );
+}
+
+sub hooks { }
+
+sub hello {
+    my $self = shift;
+    @_ = qw(world) if !@_;
+    print STDERR "Hello @_\n";
+}
+
+1;
